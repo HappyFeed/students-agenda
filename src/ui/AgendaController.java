6ba70666af9c1b4c26fde0d8a2aaf6d62d4443f7
jairoRdgz <jairitox2000@gmail.com>
2020-02-05 15:47:45 -0500

    small cahnges

--- src/ui/AgendaController.java
+++ src/ui/AgendaController.java
@@ -1,5 +1,7 @@
 package ui;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -72,32 +74,48 @@ public class AgendaController {
 
     @FXML
     private Label contactNumber;
-
+    
     @FXML
-    void initialize() {
-    	friends= new ArrayList<Contact>();
-    	buttonGuardar.setVisible(false);
-    	labelApellidoContacto.setDisable(true);
-    	labelCodigo.setDisable(true);
-    	labelEdadContacto.setDisable(true);
-    	labelNombreContacto.setDisable(true);
-    	labelSemestre.setDisable(true);
-    	LabelCarrera.setDisable(true);
-    	fotoContact.setDisable(true);
-    	contactNumber.setText("1");
-    }
+    private TextField barraBusqueda;
     
-    
+    ObservableList<String> list = FXCollections.observableArrayList("Modelado","Discretas");
+
     @FXML
     void buscarContacto(ActionEvent event) {
-
+    	String parametroBusqueda = comboBoxBusqueda.getValue();
+    	int value;
+    	switch(parametroBusqueda) {
+    		case "Nombre":
+    			value = binarySearchName(friends, 0, friends.size()-1, barraBusqueda.getText());
+    			break;
+    		case "Apellido":
+    			value = binarySearchLastName(friends, 0, friends.size()-1, barraBusqueda.getText());
+    			break;
+    		case "Edad":
+    			value = binarySearchAge(friends, 0, friends.size()-1, Integer.parseInt(barraBusqueda.getText()));
+    			break;
+    		case "Codigo":
+    			value = binarySearchCode(friends, 0, friends.size()-1, barraBusqueda.getText());
+    			break;
+    		case "Semestre":
+    			value = binarySearchSemester(friends, 0, friends.size()-1, barraBusqueda.getText());
+    			break;
+    		default:
+    			value = binarySearchName(friends, 0, friends.size()-1, barraBusqueda.getText());
+    			break;
+    	}
+    	if(value==-1) {
+    		System.out.println("No Existe");
+    	}else {
+    		friends.get(value).getName();
+    	}
     }
+ 
 
     @FXML
     void cerrarAgenda(ActionEvent event) {
     	System.exit(0);
     }
-
     @FXML
     void delatedData(ActionEvent event) {
 
@@ -176,5 +194,106 @@ public class AgendaController {
     	}
     }
     
+    public int binarySearchName(ArrayList<Contact> friends, int l, int r, String x){ 
+        if (r >= l) { 
+            int mid = l + (r - l) / 2; 
+
+            if (friends.get(mid).getName().equalsIgnoreCase(x)) {
+                return mid; 
+            }
+            if (friends.get(mid).getName().compareTo(x) > 0) { 
+                return binarySearchName(friends, l, mid - 1, x); 
+            }
+            return binarySearchName(friends, mid + 1, r, x); 
+        } 
+        return -1; 
+    }
+    
+    public int binarySearchLastName(ArrayList<Contact> friends, int l, int r, String x){ 
+        if (r >= l) { 
+            int mid = l + (r - l) / 2; 
+
+            if (friends.get(mid).getLastName().equalsIgnoreCase(x)) {
+                return mid; 
+            }
+            if (friends.get(mid).getLastName().compareTo(x) > 0) { 
+                return binarySearchLastName(friends, l, mid - 1, x); 
+            }
+            return binarySearchLastName(friends, mid + 1, r, x); 
+        }  
+        return -1; 
+    }
+    
+    public int binarySearchAge(ArrayList<Contact> friends, int l, int r, int x){ 
+        if (r >= l) { 
+            int mid = l + (r - l) / 2; 
+
+            if (friends.get(mid).getAge() == x) {
+                return mid; 
+            }
+            if (friends.get(mid).getAge() > x) { 
+                return binarySearchAge(friends, l, mid - 1, x); 
+            }
+            return binarySearchAge(friends, mid + 1, r, x); 
+        } 
+        return -1; 
+    }
+    
+    public int binarySearchCode(ArrayList<Contact> friends, int l, int r, String x){ 
+        if (r >= l) { 
+            int mid = l + (r - l) / 2; 
+
+            if (friends.get(mid).getCode().equalsIgnoreCase(x)) {
+                return mid; 
+            }
+            if (friends.get(mid).getCode().compareTo(x) > 0) { 
+                return binarySearchCode(friends, l, mid - 1, x); 
+            }
+            return binarySearchCode(friends, mid + 1, r, x); 
+        }  
+        return -1; 
+    }
+    
+    public int binarySearchSemester(ArrayList<Contact> friends, int l, int r, String x){ 
+        if (r >= l) { 
+            int mid = l + (r - l) / 2; 
+
+            if (friends.get(mid).getSemester().equalsIgnoreCase(x)) {
+                return mid; 
+            }
+            if (friends.get(mid).getSemester().compareTo(x) > 0) { 
+                return binarySearchSemester(friends, l, mid - 1, x); 
+            }
+            return binarySearchSemester(friends, mid + 1, r, x); 
+        }  
+        return -1; 
+    }
+    
+    
+    
+    @FXML
+    void initialize() {
+    	friends= new ArrayList<Contact>();
+    	buttonGuardar.setVisible(false);
+    	labelApellidoContacto.setDisable(true);
+    	labelCodigo.setDisable(true);
+    	labelEdadContacto.setDisable(true);
+    	labelNombreContacto.setDisable(true);
+    	labelSemestre.setDisable(true);
+    	LabelCarrera.setDisable(true);
+    	fotoContact.setDisable(true);
+    	contactNumber.setText("1");
+    	
+    	//ComboBox Information: the paramater which is going to be searching
+    	comboBoxBusqueda.getItems().add("Nombre");
+    	comboBoxBusqueda.getItems().add("Apellido");
+    	comboBoxBusqueda.getItems().add("Edad");
+    	comboBoxBusqueda.getItems().add("Codigo");
+    	comboBoxBusqueda.getItems().add("Semestre");
+    	
+    	//The information for the listView where the contacts are going to be shown
+    	textAreaMaterias.setItems(list);
+    	
+    }
 }
 
